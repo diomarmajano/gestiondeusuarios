2c391d0b7afe31e35c05a3dcf2a6a07bbbb4c891
Diomar Majano <http.diomar@gmail.com>
2025-05-08 00:01:49 -0400

    "creacion de test para el controlador de usuarios"

--- src/test/java/gestion/usuarios/gestiondeusuarios/controller/userControllerTest.java
+++ src/test/java/gestion/usuarios/gestiondeusuarios/controller/userControllerTest.java
@@ -6,10 +6,14 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
 import org.hamcrest.Matchers;
+import org.springframework.http.MediaType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -23,6 +27,9 @@ import gestion.usuarios.gestiondeusuarios.service.userService;
 public class userControllerTest {
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @MockBean
     private userService usuariosServiceMock;
@@ -40,10 +47,10 @@ public class userControllerTest {
         user.setRol("admin");
     }
 
+    //Prueba para la peticion HHTP GET
     @Test
     public void testGetAllUser() throws Exception {
         List<Usuarios> usuarios = Arrays.asList(user);
-
         when(usuariosServiceMock.getAllUsers()).thenReturn(usuarios);
 
         mockMvc.perform(MockMvcRequestBuilders.get("/usuarios"))
@@ -56,4 +63,38 @@ public class userControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$._embedded.usuariosList[0].direccion", Matchers.is(user.getDireccion())))
                 .andExpect(MockMvcResultMatchers.jsonPath("$._embedded.usuariosList[0].rol", Matchers.is(user.getRol())));
     }
+
+    //Prueba para la peticion HHTP GET con el id del usuario
+    @Test
+    public void testGetUserById() throws Exception {
+        Long userId = 1L;
+        when(usuariosServiceMock.findUserById(userId)).thenReturn(java.util.Optional.of(user));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/usuarios/{id}", userId))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.nombre", Matchers.is(user.getNombre())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.apellido", Matchers.is(user.getApellido())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email", Matchers.is(user.getEmail())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.password", Matchers.is(user.getPassword())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.direccion", Matchers.is(user.getDireccion())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.rol", Matchers.is(user.getRol())));
+}
+
+    //Prueba para la peticion HHTP POST
+    @Test
+    public void createUser() throws Exception {
+        when(usuariosServiceMock.createUser(user)).thenReturn(user);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/usuarios")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(user)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.nombre", Matchers.is(user.getNombre())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.apellido", Matchers.is(user.getApellido())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email", Matchers.is(user.getEmail())))     
+                .andExpect(MockMvcResultMatchers.jsonPath("$.password", Matchers.is(user.getPassword())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.direccion", Matchers.is(user.getDireccion())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.rol", Matchers.is(user.getRol())));
+            }
+
 }
